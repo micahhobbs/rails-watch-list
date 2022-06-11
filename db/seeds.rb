d39fdb6e2674ccdb7745ee99bb0c951ea75f4782
Micah Hobbs <micahthobbs@gmail.com>
2022-06-11 20:59:58 +1000

    Lists MVC done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+buffer = URI.open("https://tmdb.lewagon.com/movie/popular?&language=en-US").read
+
+movie_hash = JSON.parse(buffer)
+
+movie_hash['results'].each do |movie|
+  puts "Creating #{movie['original_title']} in DB"
+  Movie.create!(title: movie['original_title'],
+                overview: movie['overview'],
+                poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",
+                rating: movie['vote_average'].round)
+end
